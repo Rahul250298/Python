c799de9dcffa1dc6dc27981d1a9fda4906c96e7d
Rahul <rahulravi2502@gmail.com>
2022-05-05 11:44:10 +0530

    Adding : Prime factors

--- Development/functional programs/Distance.py
+++ Development/functional programs/Distance.py
@@ -1,8 +1,8 @@
 import math
 
 def Distance(x1,y1,x2,y2):
-    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    print("Disatnce = ", dist)
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    print("Distance = ", distance)
 
 x1 = int(input("x1 : "))
 y1 = int(input("y1 : "))
