319bdfe94c38e17db8989c408f593bc4eb56b576
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-05-06 12:05:00 +0530

    Adding: coupon Numbers

--- Development/logical programs/CouponNumber.py
+++ Development/logical programs/CouponNumber.py
@@ -2,10 +2,16 @@ import random
 
 def couponNumber(N):
     count=0
-    for i in range(200):
+   for i in range(200):
         randomNum=random.randint(5,100)
         if(randomNum==N):
             count+=1
     print("The coupon Number ", N ,"has repeated", count,"times")
 N = int(input("Enter a Number :"))
 couponNumber(N)        
+
+
+
+
+
+
